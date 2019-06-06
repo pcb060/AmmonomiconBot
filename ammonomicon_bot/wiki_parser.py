621b1c2c0f55c95544dac8f0b7d3335e7f96b437
Jacopo <pcb060@gmail.com>
2019-06-06 17:53:17 +0200

    Simple documentation, refactoring

--- ammonomicon_bot/wiki_parser.py
+++ ammonomicon_bot/wiki_parser.py
@@ -2,10 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 from ammonomicon_bot.conf.endpoints import *
 import ammonomicon_bot.db_manager as dbm
+from ammonomicon_bot.utils import row_to_list_of_cells
 
 
 def parse_enemies():
-    """Parse content inside enemies' table @ enterthegungeon.gamepedia.com/Enemies
+    """Parse content inside enemies' table @ http://enterthegungeon.gamepedia.com/Enemies
     """
     page = requests.get(ENEMIES_ENDPOINT).content
     soup = BeautifulSoup(page, "html.parser")
@@ -14,11 +15,11 @@ def parse_enemies():
     rows.pop(0)  # removes table header containing field names
 
     for row in rows:
-        dbm.load_parsed_enemy_into_db(row)
+        dbm.upsert_enemy(row_to_list_of_cells(row))
 
 
 def parse_guns():
-    """Parse content inside guns' table @ enterthegungeon.gamepedia.com/Guns
+    """Parse content inside guns' table @ http://enterthegungeon.gamepedia.com/Guns
     """
     page = requests.get(GUNS_ENDPOINT).content
     soup = BeautifulSoup(page, "html.parser")
@@ -27,11 +28,11 @@ def parse_guns():
     rows.pop(0)  # removes table header containing field names
 
     for row in rows:
-        dbm.load_parsed_gun_into_db(row)
+        dbm.upsert_gun(row_to_list_of_cells(row))
 
 
 def parse_items():
-    """Parse content inside items' table @ enterthegungeon.gamepedia.com/Items
+    """Parse content inside items' table @ http://enterthegungeon.gamepedia.com/Items
     """
     page = requests.get(ITEMS_ENDPOINT).content
     soup = BeautifulSoup(page, "html.parser")
@@ -40,4 +41,4 @@ def parse_items():
     rows.pop(0)  # removes table header containing field names
 
     for row in rows:
-        dbm.load_parsed_item_into_db(row)
+        dbm.upsert_item(row_to_list_of_cells(row))
