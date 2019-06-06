80f0717311583a6bac94cc6a83f12c13b5672284
Jacopo <pcb060@gmail.com>
2019-06-06 17:50:55 +0200

    Unified tables

--- ammonomicon_bot/db_manager.py
+++ ammonomicon_bot/db_manager.py
@@ -1,80 +1,105 @@
 from tinydb import TinyDB, Query
+from fuzzywuzzy import fuzz
+from ammonomicon_bot.utils import check_if_infinite, check_if_multiple_qualities
 
 db = TinyDB("ammonomicon_bot/dbs/db.json")
 
-# initialize tables
-enemies_table = db.table("Enemies")
-guns_table = db.table("Guns")
-items_table = db.table("Items")
+last_item_inserted = None
 
 
 # Icon | Name | Base Health | Description
-def load_parsed_enemy_into_db(parsed_entry):
+def upsert_enemy(parsed_entry):
+    """Query the db for an entry of type "enemy" and update it if it already exists, insert it if it doesn't
+    """
     Enemy = Query()
 
-    tmp = list()
-    for cell in parsed_entry:
-        if str(cell) != "\n":
-            tmp.append(cell)
-
     this_enemy = {
-        "Icon": tmp[0].find("img")["src"],
-        "Name": tmp[1].get_text().replace("\n", ""),
-        "Base Health": tmp[2].get_text().replace("\n", ""),
-        "Description": tmp[3].get_text().replace("\n", ""),
+        "Category": "Enemy",
+        "Icon": parsed_entry[0].find("img")["src"],
+        "Name": parsed_entry[1].get_text().replace("\n", ""),
+        "Base Health": parsed_entry[2].get_text().replace("\n", ""),
+        "Description": parsed_entry[3].get_text().replace("\n", ""),
     }
 
-    enemies_table.upsert(this_enemy, Enemy["Name"] == this_enemy["Name"])
+    db.upsert(this_enemy, Enemy["Name"] == this_enemy["Name"])
 
 
 # Icon | Name | Quote | Qlty | Type | MagSize | AmmoCap | Dmg | FireRate | RldTime | ShotSpd | Rng | Frc | Sprd | Notes
-def load_parsed_gun_into_db(parsed_entry):
+def upsert_gun(parsed_entry):
+    """Query the db for an entry of type "gun" and update it if it already exists, insert it if it doesn't
+    """
     Gun = Query()
 
-    tmp = list()
-    for cell in parsed_entry:
-        cell = str(cell)
-        if cell != "\n":
-            tmp.append(cell)
-
-    gun = {
-        "Icon": tmp[0],
-        "Name": tmp[1],
-        "Quote": tmp[2],
-        "Quality": tmp[3],
-        "Type": tmp[4],
-        "Magazine Size": tmp[5],
-        "Ammo Capacity": tmp[6],
-        "Damage": tmp[7],
-        "Fire Rate": tmp[8],
-        "Reload Time": tmp[9],
-        "Shot Speed": tmp[10],
-        "Range": tmp[10],
-        "Force": tmp[11],
-        "Spread": tmp[12],
-        "Notes": tmp[13],
+    this_gun = {
+        "Category": "Gun",
+        "Icon": parsed_entry[0].find("img")["src"],
+        "Name": parsed_entry[1].get_text().replace("\n", ""),
+        "Quote": parsed_entry[2].get_text().replace("\n", ""),
+        "Quality": parsed_entry[3].find("img")["alt"].replace(" Quality Item.png", ""),
+        "Type": parsed_entry[4].get_text().replace("\n", ""),
+        "Magazine Size": check_if_infinite(parsed_entry[5]),
+        "Ammo Capacity": check_if_infinite(parsed_entry[6]),
+        "Damage": parsed_entry[7].get_text().replace("\n", ""),
+        "Fire Rate": parsed_entry[8].get_text().replace("\n", ""),
+        "Reload Time": parsed_entry[9].get_text().replace("\n", ""),
+        "Shot Speed": check_if_infinite(parsed_entry[10]),
+        "Range": check_if_infinite(parsed_entry[11]),
+        "Force": parsed_entry[12].get_text().replace("\n", ""),
+        "Spread": parsed_entry[13].get_text().replace("\n", ""),
+        "Notes": parsed_entry[14].get_text().replace("\n", ""),
     }
 
-    guns_table.upsert(gun, Gun["Name"] == tmp[1])
+    db.upsert(this_gun, Gun["Name"] == this_gun["Name"])
 
 
 # Icon | Name | Type | Quote | Quality | Effect
-def load_parsed_item_into_db(parsed_entry):
+def upsert_item(parsed_entry):
+    """Query the db for an entry of type "item" and update it if it already exists, insert it if it doesn't
+    """
     Item = Query()
+    global last_item_inserted
 
-    tmp = list()
-    for cell in parsed_entry:
-        cell = str(cell)
-        if cell != "\n":
-            tmp.append(cell)
+    # default case
+    if len(parsed_entry) == 6:
+        this_item = {
+            "Category": "Item",
+            "Icon": parsed_entry[0].find("img")["src"],
+            "Name": parsed_entry[1].get_text().replace("\n", ""),
+            "Type": parsed_entry[2].get_text().replace("\n", ""),
+            "Quote": parsed_entry[3].get_text().replace("\n", ""),
+            "Quality": check_if_multiple_qualities(parsed_entry[4]),
+            "Effect": parsed_entry[5].get_text().replace("\n", ""),
+        }
+    # ruby bracelet special case
+    elif last_item_inserted["Name"] == "Ruby Bracelet":
+        this_item = {
+            "Category": "Item",
+            "Icon": last_item_inserted["Icon"],
+            "Name": last_item_inserted["Name"] + "(Upgraded)",
+            "Type": last_item_inserted["Type"],
+            "Quote": parsed_entry[0].get_text().replace("\n", ""),
+            "Quality": last_item_inserted["Quality"],
+            "Effect": parsed_entry[1].get_text().replace("\n", ""),
+        }
 
-    item = {
-        "Icon": tmp[0],
-        "Name": tmp[1],
-        "Type": tmp[2],
-        "Quote": tmp[3],
-        "Quality": tmp[4],
-        "Effect": tmp[5],
-    }
+    db.upsert(
+        this_item,
+        Item["Name"] == this_item["Name"] and Item["Quote"] == this_item["Quote"],
+    )
+    last_item_inserted = this_item
 
-    items_table.upsert(item, Item["Name"] == tmp[1])
+
+def get_entry(name):
+    Entry = Query()
+    res = db.all()
+
+    max = None
+    most_likely = None
+    for match in res:
+        f = fuzz.ratio(res[match]["Name"], name)
+        if f > max:
+            max = f
+            most_likely = res[match]
+
+    return most_likely
+
