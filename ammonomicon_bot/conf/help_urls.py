be6e3dfa5def7aab287f170ed064a78ad2be881d
Jacopo <pcb060@gmail.com>
2019-06-20 21:30:04 +0200

    Added config files for txt locations and help urls

---
+++ ammonomicon_bot/conf/help_urls.py
@@ -0,0 +1,4 @@
+FAQ_URL = "https://www.reddit.com/user/AmmonomiconBot/comments/c2xvm2/faq_frequently_asked_question/"
+MISTAKE_URL = "https://www.reddit.com/user/AmmonomiconBot/comments/c2xyyz/did_the_bot_do_something_wrong/"
+GITHUB_URL = "https://github.com/pcb060/AmmonomiconBot"
+SUPPORT_ME_URL = "https://ko-fi.com/pcb060"
---
+++ ammonomicon_bot/conf/txt_files.py
@@ -0,0 +1,4 @@
+FILENAME_COMMENT_IDS = "comments_replied_to.txt"
+FILENAME_UPDATE_DATE = "last_update.txt"
+PATH_TO_COMMENT_IDS = "ammonomicon_bot/data/" + FILENAME_COMMENT_IDS
+PATH_TO_UPDATE_DATE = "ammonomicon_bot/data/" + FILENAME_UPDATE_DATE
